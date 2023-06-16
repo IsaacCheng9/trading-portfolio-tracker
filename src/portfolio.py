0017796d989c206a4ef1e5fc99d289d7cd31df12
cameronstackhouse <stackhousecameron@gmail.com>
2023-06-16 10:32:58 +0100

    Reformat with black

--- src/portfolio.py
+++ src/portfolio.py
@@ -11,7 +11,7 @@ from decimal import Decimal
 import duckdb
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QMainWindow
-from PySide6.QtCore import QTimer 
+from PySide6.QtCore import QTimer
 
 from src.transactions import AddTransactionDialog
 from src.ui.main_window_ui import Ui_main_window
@@ -35,7 +35,7 @@ class MainWindow(QMainWindow, Ui_main_window):
             QtWidgets.QHeaderView.ResizeMode.ResizeToContents
         )
         self.load_portfolio_table()
-        
+
         # Creates and links timer to refresh live stock data
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_stock_prices)
@@ -108,12 +108,12 @@ class MainWindow(QMainWindow, Ui_main_window):
         # Get the current time in DD/MM/YYYY HH:MM:SS format.
         cur_time = time.strftime("%d/%m/%Y %H:%M:%S")
         self.lbl_last_updated.setText(f"Last Updated: {cur_time}")
-    
+
     def update_stock_prices(self) -> None:
         """
-        Update live stock current prices, change in value, and 
+        Update live stock current prices, change in value, and
         absolute rate of return.
-        
+
         TODO Maybe optimise this? Causes 1 sec lag when updating.
         """
         self.table_widget_portfolio.blockSignals(True)
@@ -149,8 +149,7 @@ class MainWindow(QMainWindow, Ui_main_window):
                     )
                 ),
             )
-                
-        
+
         self.table_widget_portfolio.blockSignals(False)
 
         cur_time = time.strftime("%d/%m/%Y %H:%M:%S")
